a53fbced6964564c676b17ada8d7990491d71945
soulful71 <concededsinister@gmail.com>
2026-02-14 16:28:39 -0500

    day 2 of work

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    
+    string jaroobed = "bankai";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log("Hello World")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
