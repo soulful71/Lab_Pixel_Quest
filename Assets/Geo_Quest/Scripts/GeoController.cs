44aa0f45baa82beb1ea15dd440755fd438cf4de9
soulful71 <concededsinister@gmail.com>
2026-02-21 16:23:52 -0500

    qd
    day5

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
-    
+
     string jaroobed = "bankai";
-    
+            int counter = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        Debug.Log("Hello World")
+
+        Debug.Log("hasdasd");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Debug.Log(counter);
+        counter++;
+        if (Input.GetKeyDown(KeyCode.W)) ;
+        transform.position += new Vector3(0, 1, 0);
     }
 }
+    
+
